87b642976d420562349691126b963af8c28e8282
vitt2411 <vitt2411@gmail.com>
2021-03-12 11:24:57 +0530

    3rd changes

--- CarFinanceDisbursementModule/src/main/java/com/cjc/app/carLoan/disbursement/CarFinanceDisbursementModuleApplication.java
+++ CarFinanceDisbursementModule/src/main/java/com/cjc/app/carLoan/disbursement/CarFinanceDisbursementModuleApplication.java
@@ -9,6 +9,7 @@ public class CarFinanceDisbursementModuleApplication
 
 	public static void main(String[] args)
 	{System.out.println("hiiiiiiiiii");
+	System.out.println("hi");
 		SpringApplication.run(CarFinanceDisbursementModuleApplication.class, args);
 	}
 
